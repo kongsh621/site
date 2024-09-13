b44e073533a52bb9aecd56c3d8e89c750635f5b5
kongsh621 <147482650+kongsh621@users.noreply.github.com>
2024-09-13 11:00:41 +0900

    Update RedirectMessage.java
    잘못 올림

--- java/com/example/project01/service/RedirectMessage.java
+++ java/com/example/project01/service/RedirectMessage.java
@@ -3,22 +3,25 @@ package com.example.project01.service;
 import com.example.project01.domain.Criteria;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Service
 public class RedirectMessage {
-    public void sendRedirect(RedirectAttributes ra, Long id, Criteria criteria,
-                             String message, Model model, String name, Object object){
+    public String sendRedirect(RedirectAttributes ra, Long id, Criteria criteria,
+                               String message, String redirectUrl){
         ra.addAttribute("id", id);
-        ra.addAttribute("page", criteria.getPage());
         ra.addAttribute("message", message);
-        model.addAttribute(name, object);
+        if (redirectUrl != null && !redirectUrl.contains("index")) {
+            ra.addAttribute("page", criteria.getPage());
+            return "redirect:" + redirectUrl;
+        }
+        // 댓글 페이지는?
+        return "redirect:/";
     }
-    // id 없는 버전 (로그인)
-    public void sendRedirectExceptId(RedirectAttributes ra, Criteria criteria,
-                             String message, Model model, String name, Object object){
-        ra.addAttribute("page", criteria.getPage());
+    // id 없는 버전 (로그인) ajax로 위치 읽어와서 페이지 포함
+     public String  sendRedirectExceptId(RedirectAttributes ra, String message, String redirectUrl){
         ra.addAttribute("message", message);
-        model.addAttribute(name, object);
+        return "redirect:" + redirectUrl;
     }
 }
