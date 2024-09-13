08e3ac5f6b2944d0c55b33ab118d7f7b468a501f
kongsh621 <147482650+kongsh621@users.noreply.github.com>
2024-09-13 13:21:23 +0900

    Update RedirectMessage.java
    필요에 따라 기능 수정

--- java/com/example/project01/service/RedirectMessage.java
+++ java/com/example/project01/service/RedirectMessage.java
@@ -8,17 +8,25 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Service
 public class RedirectMessage {
-    public String sendRedirect(RedirectAttributes ra, Long id, Criteria criteria,
+    public String sendRedirect(RedirectAttributes ra,
+                               @RequestParam(name="id", required = false)Long id, Criteria criteria,
                                String message, String redirectUrl){
-        ra.addAttribute("id", id);
+        // id만 필요없을 때(인포보드에서 찜할 때)
+        if (id != null)
+            ra.addAttribute("id", id);
+
         ra.addAttribute("message", message);
+
+        if (criteria == null){ // 페이지 필요 없을 때
+            return "redirect:" + redirectUrl;
+        }
         if (redirectUrl != null && !redirectUrl.contains("index")) {
             ra.addAttribute("page", criteria.getPage());
             return "redirect:" + redirectUrl;
         }
         return "redirect:/";
     }
-    // id 없는 버전 (로그인) ajax로 위치 읽어와서 페이지 포함
+    // id, page 둘 다 없는 버전 (로그인) ajax로 위치 읽어와서 페이지 포함 / 검색
      public String  sendRedirectExceptId(RedirectAttributes ra, String message, String redirectUrl){
         ra.addAttribute("message", message);
         return "redirect:" + redirectUrl;
