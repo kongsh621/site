be6f250e18d998baa04aea336eb8b6f3d56134bd
kongsh621 <147482650+kongsh621@users.noreply.github.com>
2024-09-13 16:27:44 +0900

    Update BoardController.java

--- java/com/example/project01/controller/BoardController.java
+++ java/com/example/project01/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.project01.controller;
 
 import com.example.project01.domain.*;
 import com.example.project01.service.BoardService;
+import com.example.project01.service.RedirectMessage;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -29,6 +30,10 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    @Autowired
+    private RedirectMessage redirectMessage;
+
+
     @GetMapping({"/list"})
     public String list(Model model, Criteria criteria, @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember,
                        @ModelAttribute BoardVO boardVO) {
@@ -36,6 +41,7 @@ public class BoardController {
         model.addAttribute("loginSession", loginMember);
 
         long total = boardService.getTotal();
+
         log.info("total = {}", total);
 
         List<BoardVO> result = boardService.getListWithPaging(criteria);
@@ -46,6 +52,7 @@ public class BoardController {
             model.addAttribute("admin", loginMember);
             System.out.println("관리자입니다.");
         }
+
         return "siteForMember";
     }
 
@@ -54,11 +61,12 @@ public class BoardController {
                         @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember,
                         HttpServletRequest request) throws IOException {
 
+
         log.info("write = {}", board);
         model.addAttribute("loginSession", loginMember);
-        log.info("-----------파일 받고 바로" +file.getOriginalFilename());
-        
         // 파일은 board가 아니라 MultipartFile 로 읽어와서 처리 후 데이터베이스에 추가한다
+
+        log.info("-----------파일 받고 바로" +file.getOriginalFilename());
         if (file != null && !file.isEmpty()) { // 첨부파일이 있다면
             // 첨부파일 저장 경로
             String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
@@ -84,7 +92,9 @@ public class BoardController {
         }
 
         redirectAttributes.addAttribute("id", board.getId());
-        redirectAttributes.addFlashAttribute("result", new ResultDTO(true, "write"));
+        redirectAttributes.addFlashAttribute("result",
+            new ResultDTO(true, "write"));
+
 
         return "redirect:/read";
     }
@@ -105,65 +115,64 @@ public class BoardController {
     @GetMapping("/read")
     public String read(@RequestParam Long id, Criteria criteria, Model model, HttpServletRequest request,
                        @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember,
-                       HttpServletResponse response) {
+                       HttpServletResponse response, RedirectAttributes ra) {
         log.info("read = {}", id);
         model.addAttribute("loginSession", loginMember);
 
-        long total = boardService.getTotal();
-        model.addAttribute("pageMaker", new PageMaker(criteria, total));
+        try {
+            // get 메서드를 이용해 id로 게시글 정보를 불러온다
+            BoardVO boardResult = boardService.get(id);
 
-        // get 메서드를 이용해 id로 게시글 정보를 불러온다
-        BoardVO boardResult = boardService.get(id);
-        log.info("board = " + boardResult);
+            // 파일이 존재하면 이미지로 출력
+            // 다운 버튼 표시
+            if (boardResult.getFilename() != null && !boardResult.getFilename().isEmpty()){
+                log.info("파일이 존재" + boardResult.getFilename());
+                model.addAttribute("imgSession", boardResult.getFilepath());
+                model.addAttribute("attachedFile", boardResult);
+            }
 
-        // 파일이 존재하면 이미지로 출력
-        if (boardResult.getFilename() != null ) {
-            model.addAttribute("imgSession", boardResult.getFilepath());
+            // 만약 회원이라면, 작성자일 때 writer 세션에 저장해서 다른 양식 출력
+            if (loginMember != null && loginMember.getNickname().equals(boardResult.getWriter())) {
+                // 일단 null인지부터 확인해줘야 오류 안 난다
+                model.addAttribute("writer", loginMember);
+            }
+
+            // 댓글 출력
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("criteria", criteria);
+            map.put("postid", id);
+
+            // 댓글 불러와서 list로 보냄
+            List<BoardCommentVO> list = boardService.getCommentWithPaging(map);
+            model.addAttribute("list", list);
+
+            long total = boardService.getTotal();
+            model.addAttribute("pageMaker", new PageMaker(criteria, total));
+
+            long comTotal = boardService.getCommentTotal(id);
+            model.addAttribute("commentPageMaker", new PageMaker(criteria, comTotal));
+
+            // 조회수 (회원/비회원 동일하게 1번씩만 조회수 집계, 브라우저 종료시 초기화 되는 단점이 있다.)
+            Cookie viewCount = addHits(request, id, response);
+            if (viewCount != null)
+                response.addCookie(viewCount);
+
+            model.addAttribute("board", boardResult);
+            return "siteForMemberRead";
+        } catch (Exception e){
+            return redirectMessage.sendRedirectExceptId(ra, "시스템 오류가 발생했습니다.", "/list");
         }
-
-        // 첨부파일 존재하면 다운 버튼 표시
-        if (boardResult.getFilename() != null && !boardResult.getFilename().isEmpty()){
-            log.info("파일이 존재" + boardResult.getFilename());
-            System.out.println("/static"+boardResult.getFilepath());
-            model.addAttribute("attachedFile", boardResult);
-        }
-
-
-        // 만약 회원이라면, 작성자일 때 writer 세션에 저장해서 다른 양식 출력
-        if (loginMember != null && loginMember.getNickname().equals(boardResult.getWriter())) { // 일단 null인지부터 확인해줘야 오류 안 난다
-            model.addAttribute("writer", loginMember);
-        }
-
-        // 댓글 출력
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("criteria", criteria);
-        map.put("postid", id);
-        List<BoardCommentVO> list = boardService.getCommentWithPaging(map);
-        // 댓글 불러와서 list로 보냄
-        model.addAttribute("list", list);
-        System.out.println("----------"+list);
-
-        long comTotal = boardService.getCommentTotal(id);
-        model.addAttribute("commentPageMaker", new PageMaker(criteria, comTotal));
-
-        // 조회수 (회원/비회원 동일하게 1번씩만 조회수 집계, 브라우저 종료시 초기화 되는 단점이 있다.)
-        Cookie viewCount = addHits(request, id, response);
-        if (viewCount != null)
-            response.addCookie(viewCount);
-
-        model.addAttribute("board", boardResult);
-        return "siteForMemberRead";
     }
 
     // 조회수 1 증가
     public Cookie addHits(HttpServletRequest request, Long id, HttpServletResponse response) {
         Cookie[] cookies = request.getCookies();
         System.out.println("cookies = " + Arrays.toString(cookies));
+
         if (cookies == null) { // 쿠키 없으면 생성
             boardService.updateHits(id);
             Cookie newCookie = new Cookie("view_count", id.toString());
             newCookie.setPath("/");
-            System.out.println("Set new Cookie");
             return newCookie;
         } else {
             for (Cookie cookie : cookies) {
@@ -172,7 +181,6 @@ public class BoardController {
                         // 쿠키에 조회한 기록 없으면 조회수 +1
                         boardService.updateHits(id);
                         cookie.setValue(cookie.getValue() + "_" + id);
-                        System.out.println("Add new id in Cookie");
                         return cookie;
                     } else return null;
                 }
@@ -181,64 +189,59 @@ public class BoardController {
             boardService.updateHits(id);
             Cookie newCookie = new Cookie("view_count", id.toString());
             newCookie.setPath("/");
-            System.out.println("Set new Cookie");
             return newCookie;
         }
     }
-    
-//     폼으로 보내서 서버에서 처리하는 방법
+
     @RequestMapping("/writeComment")
     public String writeBoardComment(@ModelAttribute BoardCommentVO boardCommentVO, RedirectAttributes ra,
                                     @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember,
                                      Model model, @RequestParam int comPage){
-        // comment에 필요한 거 title, content, writer, regdate
-        // 포워딩 한 거 postid, content, writer
         model.addAttribute("loginSession", loginMember);
-        System.out.println("boardComment writer = " + boardCommentVO.getWriter());
-        
-        boolean result = boardService.writeComment(boardCommentVO); // 새 댓글 추가
-        // 댓글 페이징
-        if (result){
-            ra.addAttribute("id", boardCommentVO.getPostid()); // comment 아니고 board id다
-            ra.addAttribute("comPage", comPage);
-            ra.addAttribute("message", "댓글을 작성하였습니다.");
-            model.addAttribute("comWriter", loginMember.getNickname());
-            return "redirect:/read";
+
+        try {
+            boolean result = boardService.writeComment(boardCommentVO); // 새 댓글 추가
+            Long postId = boardCommentVO.getPostid();
+            Criteria criteria = new Criteria(comPage, 10, null);
+
+            // 댓글 페이징
+            if (result){
+                return redirectMessage.sendRedirect(ra, postId, criteria, "댓글을 작성하였습니다.", "/read");
+            }
+            return redirectMessage.sendRedirect(ra, postId, criteria, "댓글 작성에 실패했습니다.", "/read");
+        } catch (Exception e){
+            return redirectMessage.sendRedirect(ra, boardCommentVO.getPostid(), null, "시스템 오류가 발생했습니다.", "/read");
         }
-        ra.addAttribute("id", boardCommentVO.getPostid());
-        ra.addAttribute("message", "댓글 작성에 실패했습니다.");
-        return "redirect:/read";
     }
 
     @PostMapping("/update")
-    public String update(BoardVO board, Criteria criteria, RedirectAttributes redirectAttributes,
+    public String update(BoardVO board, Criteria criteria, RedirectAttributes ra,
                          @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember, Model model) {
         log.info("update = {}", board);
         model.addAttribute("loginSession", loginMember);
 
-        if (boardService.update(board)) {
-            redirectAttributes.addFlashAttribute("result",
-                new ResultDTO(true, "update"));
+        try {
+            Long postId = board.getId();
+            if (boardService.update(board)) {
+                ra.addFlashAttribute("result", new ResultDTO(true, "update"));
+            }
+            return redirectMessage.sendRedirect(ra, postId, criteria, "업데이트 되었습니다.", "/read");
+        } catch (Exception e){
+            return redirectMessage.sendRedirect(ra, board.getId(), criteria, "시스템 오류가 발생했습니다.", "/read");
         }
-
-        redirectAttributes.addAttribute("id", board.getId());
-        redirectAttributes.addAttribute("page", criteria.getPage());
-
-        return "redirect:/read";
     }
 
     @GetMapping("/update")
-    public String update(@RequestParam("id") Long id, Criteria criteria, Model model,
+    public String update(@RequestParam("id") Long id, Model model,
                          @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember) {
         log.info("update = {}", id);
         model.addAttribute("loginSession", loginMember);
         model.addAttribute("board", boardService.get(id));
-
         return "siteForMemberWrite";
     }
 
     @RequestMapping("/delete")
-    public String delete(@RequestParam("id") Long id, Criteria criteria, RedirectAttributes redirectAttributes,
+    public String delete(@RequestParam("id") Long id, Criteria criteria, RedirectAttributes ra,
                          @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember, Model model)  {
         log.info("delete = {}", id);
         model.addAttribute("loginSession", loginMember);
@@ -246,7 +249,7 @@ public class BoardController {
         BoardVO file = boardService.findFiles(id);
 
         if (boardService.delete(id))
-            redirectAttributes.addFlashAttribute("result",
+            ra.addFlashAttribute("result",
                 new ResultDTO(true, "delete"));
 
         // 파일이 존재하면
@@ -261,9 +264,8 @@ public class BoardController {
                     System.out.println("파일이 삭제되지 않았습니다");
                 }
             }
-            redirectAttributes.addAttribute("page", criteria.getPage());
         }
-        return "redirect:/list";
+        return redirectMessage.sendRedirect(ra, null, criteria, "게시물이 삭제되었습니다", "/list");
     }
 
     @GetMapping("/deleteComment")
@@ -272,32 +274,29 @@ public class BoardController {
         // 보낸 값들 읽어서 리다이렉트 주소에 사용
         long id = commentVO.getId();
         long postId = commentVO.getPostid();
-        int page = criteria.getPage();
 
-        boardService.deleteComment(id);
-        ra.addAttribute("id", postId);
-        ra.addAttribute("page", page);
-        ra.addAttribute("message", "댓글이 삭제되었습니다.");
-        System.out.println("commentVO = " + commentVO + ", criteria = " + criteria);
-        return "redirect:/read";
+        if (boardService.deleteComment(id))
+            return redirectMessage.sendRedirect(ra, postId, criteria, "댓글이 삭제되었습니다.", "/read");
+        else
+            return redirectMessage.sendRedirect(ra, postId, criteria, "댓글을 삭제하지 못했습니다.", "/read");
     }
 
     @RequestMapping("/search")
-    public String listSearch (@RequestParam String search, Model model, Criteria criteria, 
+    public String listSearch (@RequestParam String search, Model model, Criteria criteria,
                               @SessionAttribute(name = "loginMember", required = false) MemberVO loginMember,
-                              RedirectAttributes redirectAttributes){
+                              RedirectAttributes ra){
         model.addAttribute("loginSession", loginMember);
 
+        List<BoardVO> result = null;
         // 검색어와 페이지 객체를 맵핑해서 리스트를 받아옴
         HashMap<String, Object> map = new HashMap<>();
         map.put("search", search);
         map.put("criteria", criteria);
+
         try {
             if (search.length() <= 2) { // 3글자 이상
-                redirectAttributes.addAttribute("message", "검색어를 최소 3글자 이상 입력해 주세요.");
-                return "redirect:/list";
+                return redirectMessage.sendRedirectExceptId(ra,"검색어를 최소 3글자 이상 입력해 주세요.", "/list");
             }
-            List<BoardVO> result = null;
             result = boardService.findByText(map);
             long total = boardService.getSearchTotal(search);
 
@@ -306,10 +305,10 @@ public class BoardController {
                 model.addAttribute("pageMaker", new PageMaker(criteria, total));
                 return "/siteForMember";
             }
+            model.addAttribute("emptyResult", "Y");
+            return "/siteForMember";
         } catch (Exception e) {
-            redirectAttributes.addAttribute("message", "시스템에 문제가 발생했습니다");
-            return "redirect:/list";
+            return redirectMessage.sendRedirectExceptId(ra, "시스템에 문제가 발생했습니다.", "/list");
         }
-        return "/siteForMember";
     }
 }
